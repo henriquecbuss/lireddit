79f7a9d5434e79bae29bdd4e381f5479dec5f227
Henrique Buss <henrique.buss@hotmail.com>
2020-12-09 15:54:17 -0300

    Added form

--- src/Page/Register.elm
+++ src/Page/Register.elm
@@ -2,7 +2,11 @@ module Page.Register exposing (..)
 
 import Browser
 import Element exposing (..)
-import Html exposing (Html)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html, input)
 import Session exposing (Session)
 
 
@@ -11,12 +15,17 @@ import Session exposing (Session)
 
 
 type alias Model =
-    { session : Session }
+    { session : Session
+    , username : String
+    , password : String
+    }
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( { session = session }, Cmd.none )
+    ( { session = session, username = "", password = "" }
+    , Cmd.none
+    )
 
 
 
@@ -24,7 +33,9 @@ init session =
 
 
 type Msg
-    = NoOp
+    = ChangedUsername String
+    | ChangedPassword String
+    | Submitted
 
 
 
@@ -33,9 +44,44 @@ type Msg
 
 view : Model -> Browser.Document Msg
 view model =
+    let
+        inputFieldWidth =
+            maximum 800 fill
+    in
     { title = "Register"
     , body =
-        [ layout [] (el [] (text "register"))
+        [ layout []
+            (column
+                [ height fill
+                , width <| maximum 800 fill
+                , paddingXY 80 80
+                , spacing 30
+                , centerX
+                ]
+                [ Input.username [ Input.focusedOnLoad ]
+                    { onChange = ChangedUsername
+                    , text = model.username
+                    , placeholder = Just (Input.placeholder [] (text "username"))
+                    , label = Input.labelAbove [] (text "Username")
+                    }
+                , Input.newPassword []
+                    { onChange = ChangedPassword
+                    , text = model.password
+                    , placeholder = Just (Input.placeholder [] (text "password"))
+                    , label = Input.labelAbove [] (text "Password")
+                    , show = model.password == ""
+                    }
+                , Input.button
+                    [ Background.color <| rgb255 0 128 128
+                    , Font.color <| rgb 1 1 1
+                    , Border.rounded 4
+                    , paddingXY 30 15
+                    ]
+                    { onPress = Just Submitted
+                    , label = el [ centerX, centerY ] <| text "Register"
+                    }
+                ]
+            )
         ]
     }
 
@@ -47,7 +93,14 @@ view model =
 update : Model -> Msg -> ( Model, Cmd Msg )
 update model msg =
     case msg of
-        NoOp ->
+        ChangedUsername username ->
+            ( { model | username = username }, Cmd.none )
+
+        ChangedPassword password ->
+            ( { model | password = password }, Cmd.none )
+
+        Submitted ->
+            -- TODO
             ( model, Cmd.none )
 
 
